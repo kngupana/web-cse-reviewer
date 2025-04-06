4e295c032a47f26d294d3bd35d59a70b63cc26b8
kngupana <gupanakate@gmail.com>
2025-04-06 16:06:56 +0800

    fix: adjust color of click to login and register

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -4,6 +4,8 @@
 
     <v-text-field label="Password" typw="password" variant="outlined"></v-text-field>
 
-    <v-btn class="mt-2" type="submit" block color="primary" prepend-icon="mdi-login">Login</v-btn>
+    <v-btn class="mt-2" type="submit" block color="purple-darken-3" prepend-icon="mdi-login"
+      >Login</v-btn
+    >
   </v-form>
 </template>
