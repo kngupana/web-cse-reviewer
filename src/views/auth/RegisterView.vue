e280b371f1d2230a3b4c4178874a4ea38c9b0956
kngupana <gupanakate@gmail.com>
2025-04-06 13:24:45 +0800

    feat: added login and register page

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -26,17 +26,31 @@ function onClick() {
         <v-container>
           <v-row>
             <v-col cols="12" md="6" class="mx-auto pt-16">
-              <v-card class="mx-auto" prepend-icon="mdi-login" subtitle="Login" elevation="24">
+              <v-card
+                class="mx-auto"
+                prepend-icon="mdi-account-plus"
+                subtitle="Register"
+                elevation="24"
+              >
                 <template v-slot:title>
                   <span class="font-weight-black">CSE Reviewer</span>
                 </template>
 
                 <v-card-text class="bg-surface-light pt-4">
                   <v-form fast-fail @submit.prevent>
+                    <v-text-field label="Firstname" variant="outlined"></v-text-field>
+                    <v-text-field label="Lastname" variant="outlined"></v-text-field>
+
                     <v-text-field label="Email" variant="outlined"></v-text-field>
 
                     <v-text-field
                       label="Password"
+                      typw="password"
+                      variant="outlined"
+                    ></v-text-field>
+
+                    <v-text-field
+                      label="Password Confirmation"
                       typw="password"
                       variant="outlined"
                     ></v-text-field>
@@ -46,10 +60,8 @@ function onClick() {
 
                   <v-divider class="my-5"> </v-divider>
                   <h5 class="text-center">
-                    Don't have account?
-                    <RouterLink class="text-primary" to="/register"
-                      >Click here to Register</RouterLink
-                    >
+                    Already have an account?
+                    <RouterLink class="text-primary" to="/">Click here to Login</RouterLink>
                   </h5>
                 </v-card-text>
               </v-card>
