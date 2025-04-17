538d472ab22e6388ce1f4e0cc3ae9e8bce811c4c
kngupana <gupanakate@gmail.com>
2025-04-17 23:29:44 +0800

    feat: new log in and register design

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,16 +1,54 @@
+<script setup>
+import { ref } from 'vue'
+
+const showPassword = ref(false)
+
+const togglePassword = () => {
+  showPassword.value = !showPassword.value
+}
+
+const handleLogin = () => {
+  // Your login logic goes here
+  console.log('Logging in...')
+}
+</script>
+
 <template>
-  <v-form fast-fail @submit.prevent>
-    <v-text-field prepend-icon="mdi-email" label="Email" variant="outlined"></v-text-field>
+  <v-container class="py-10" max-width="450px">
+    <v-card elevation="10" class="pa-6 rounded-xl">
+      <v-card-title class="text-h5 font-weight-bold text-center mb-4"> Welcome Back </v-card-title>
 
-    <v-text-field
-      prepend-icon="mdi-lock"
-      label="Password"
-      typw="password"
-      variant="outlined"
-    ></v-text-field>
+      <v-form fast-fail @submit.prevent="handleLogin">
+        <v-text-field
+          label="Email Address"
+          prepend-icon="mdi-email"
+          type="email"
+          variant="outlined"
+          density="comfortable"
+          required
+        />
 
-    <v-btn class="mt-2" type="submit" block color="purple-darken-3" prepend-icon="mdi-login"
-      >Login</v-btn
-    >
-  </v-form>
+        <v-text-field
+          v-model="password"
+          :type="showPassword ? 'text' : 'password'"
+          label="Password"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+          @click:append="togglePassword"
+          variant="outlined"
+          density="comfortable"
+          required
+        />
+
+        <v-btn type="submit" block color="purple-darken-3" class="mt-4" prepend-icon="mdi-login">
+          Login
+        </v-btn>
+        <div class="text-right">
+          <a href="#" class="forgot-password">Forgot password?</a>
+        </div>
+      </v-form>
+
+      <v-divider class="my-4" />
+    </v-card>
+  </v-container>
 </template>
--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,16 +1,103 @@
+<script setup>
+import { ref } from 'vue'
+
+const showPassword = ref(false)
+const showConfirmPassword = ref(false)
+const password = ref('')
+
+const togglePassword = () => {
+  showPassword.value = !showPassword.value
+}
+
+const toggleConfirmPassword = () => {
+  showConfirmPassword.value = !showConfirmPassword.value
+}
+
+const submitForm = () => {
+  // Add form handling logic here
+  console.log('Form submitted')
+}
+</script>
+
 <template>
-  <v-form fast-fail @submit.prevent>
-    <v-text-field label="Firstname" variant="outlined"></v-text-field>
-    <v-text-field label="Lastname" variant="outlined"></v-text-field>
+  <v-container class="py-6" max-width="500px">
+    <v-card elevation="10" class="pa-6 rounded-xl">
+      <v-card-title class="text-h5 font-weight-bold text-center pb-4">
+        Create an Account
+      </v-card-title>
 
-    <v-text-field label="Email" variant="outlined"></v-text-field>
+      <v-form fast-fail @submit.prevent="submitForm">
+        <v-row dense>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              label="First Name"
+              variant="outlined"
+              prepend-icon="mdi-account"
+              required
+            />
+          </v-col>
 
-    <v-text-field label="Password" typw="password" variant="outlined"></v-text-field>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              label="Last Name"
+              variant="outlined"
+              prepend-icon="mdi-account"
+              required
+            />
+          </v-col>
 
-    <v-text-field label="Password Confirmation" typw="password" variant="outlined"></v-text-field>
+          <v-col cols="12">
+            <v-text-field
+              label="Email"
+              variant="outlined"
+              prepend-icon="mdi-email"
+              type="email"
+              required
+            />
+          </v-col>
 
-    <v-btn class="mt-2" type="submit" block color="purple-darken-3" prepend-icon="mdi-account-plus"
-      >Register</v-btn
-    >
-  </v-form>
+          <v-col cols="12">
+            <v-text-field
+              v-model="password"
+              :type="showPassword ? 'text' : 'password'"
+              label="Password"
+              variant="outlined"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+              @click:append="togglePassword"
+              required
+            />
+          </v-col>
+
+          <v-col cols="12">
+            <v-text-field
+              :type="showConfirmPassword ? 'text' : 'password'"
+              label="Confirm Password"
+              variant="outlined"
+              prepend-icon="mdi-lock-check"
+              :append-icon="showConfirmPassword ? 'mdi-eye-off' : 'mdi-eye'"
+              @click:append="toggleConfirmPassword"
+              required
+            />
+          </v-col>
+        </v-row>
+        <v-checkbox
+          v-model="acceptTerms"
+          label="I accept the Terms and Conditions"
+          :rules="[(v) => !!v || 'You must accept the terms']"
+          required
+        />
+
+        <v-btn
+          class="mt-4"
+          type="submit"
+          block
+          color="purple-darken-3"
+          prepend-icon="mdi-account-plus"
+        >
+          Register
+        </v-btn>
+      </v-form>
+    </v-card>
+  </v-container>
 </template>
