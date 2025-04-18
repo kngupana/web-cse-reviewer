a6ec7aab249cbc3d60303c780fde1f60394c6f42
kngupana <gupanakate@gmail.com>
2025-04-19 01:13:37 +0800

    feat: added registration func

---
+++ src/components/common/AlertNotification.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+  formSuccessMessage: String,
+  formErrorMessage: String,
+})
+</script>
+
+<template>
+  <v-alert
+    v-if="formSuccessMessage"
+    :text="formSuccessMessage"
+    title="Success!"
+    type="success"
+    variant="tonal"
+    density="compact"
+    boarder="start"
+    closable
+  >
+  </v-alert>
+
+  <v-alert
+    v-if="formErrorMessage"
+    :text="formErrorMessage"
+    title="Ooops!"
+    type="error"
+    variant="tonal"
+    density="compact"
+    boarder="start"
+    closable
+  ></v-alert>
+</template>
