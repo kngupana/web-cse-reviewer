83bbff8e43e9d2e8704fee82fa890994642910ee
ruffamigultiano <ruffamaegultiano@gmail.com>
2025-04-25 01:49:53 +0800

    Added global loading overlay to LoginForm.vue

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import { ref } from 'vue'
 import AppLayout from '@/components/layout/AppLayout.vue'
 import { useDisplay } from 'vuetify'
 import LoginForm from '@/components/auth/LoginForm.vue'
 
 const { mobile } = useDisplay()
+const isLoading = ref(false)
 </script>
 
 <template>
@@ -13,27 +15,45 @@ const { mobile } = useDisplay()
         <v-col cols="12" md="6" class="mx-auto pt-16">
           <v-card class="mx-auto" elevation="24">
             <v-card-title class="text-center">
-              <v-img class="mx-auto" src="images/logo.jpg" :width="mobile ? '75%' : '25%'"></v-img>
+              <v-img
+                class="mx-auto"
+                src="images/logo.jpg"
+                :width="mobile ? '75%' : '25%'"
+              ></v-img>
               <h3 class="font-weight-black">CSE Reviewer</h3>
               <p class="font-weight-bold">Login Form</p>
             </v-card-title>
 
             <v-card-text class="bg-surface-light pt-4">
               <v-divider class="my-5"></v-divider>
-              <LoginForm></LoginForm>
-              <br />
 
-              <v-divider class="my-5"> </v-divider>
+              <!-- Pass event listener for loading -->
+              <LoginForm @update:loading="isLoading = $event" />
+
+              <br />
+              <v-divider class="my-5"></v-divider>
               <h5 class="text-center">
                 Don't have account?
-                <RouterLink color="purple-darken-3 font-weight-black" to="/register"
-                  >Click here to Register</RouterLink
+                <RouterLink
+                  color="purple-darken-3 font-weight-black"
+                  to="/register"
                 >
+                  Click here to Register
+                </RouterLink>
               </h5>
             </v-card-text>
           </v-card>
         </v-col>
       </v-row>
+
+      <!-- Global loading overlay -->
+      <v-overlay :model-value="isLoading" persistent class="d-flex align-center justify-center">
+        <v-progress-circular
+          indeterminate
+          size="64"
+          color="deep-purple-darken-3"
+        />
+      </v-overlay>
     </template>
   </AppLayout>
 </template>
